4b69c0031a1f34a5ca1a5639b5c469d642275472
Joe Chang <131396136+josephachang02@users.noreply.github.com>
2023-10-14 20:37:53 -0700

    fullstackserver

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+import { useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [meals, setMeals] = useState([]);
+
+
+    useEffect(() => {
+      // go get the data, put it in state
+      axios({
+          method: "GET",
+          url:"http://localhost:3123/meals"
+      }).then((response) => {
+        setMeals(response.data)
+      })
+      .catch((error) => {
+        console.error("error fetching data:", error);
+      });
+
+    }, [])
+
+  return (
+    <>
+      <h1>My Meals App</h1>
+      {meals.map((meal) => {
+      return (
+        <div key={JSON.stringify(meals)}>
+          
+          <div>{meal.name}</div>
+          <img src={meal.picture}/>
+          <div>{meal.ingredients}</div>
+          <div>{meal.instructions}</div>
+      </div>
+      )
+    })}
+    </>
+  )
+}
+
+export default App
